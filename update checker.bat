5409397b438d5ffd9d10719504897e8758a02728
Pikakid98 <12392651+pikakid98@users.noreply.github.com>
2022-01-13 02:25:56 +0000

    Add files via upload

--- update checker.bat
+++ update checker.bat
@@ -6,7 +6,9 @@ mkdir "Git"
 
 START /wait "" "HTTP_Downloader.exe" --portable --shutdown-action 1 --url https://github.com/pikakid98/pikakid98-launcher-data/releases/download/data/Git.7z --immediate --output-directory "%tmp%\PL-Temp"
 
-7z x "%tmp%\PL-Temp\Git.7z" -o"Git"
+https://github.com/pikakid98/pikakid98-launcher-data/releases/download/data/Git.7z
+
+7za x "%tmp%\PL-Temp\Git.7z" -o"Git"
 
 rmdir /S /Q "%tmp%\PL-Temp"
 rmdir /S /Q "C:\incomplete"
